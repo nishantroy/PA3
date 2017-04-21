36f8fb71c19b09487a311c134e5190e846b1f51b
Nishant Roy <nroy@gatech.edu>
2017-04-21 03:13:08 -0400

    Complete Setup.
    Now all we have to do is actually implement the algorithms :)

--- src/VectorRoutingSim.java
+++ src/VectorRoutingSim.java
@@ -10,54 +10,37 @@ import java.util.Scanner;
  */
 public class VectorRoutingSim {
 
-    private Network readNetwork(String fileName) throws Exception {
-        Scanner scanner = new Scanner(new File(fileName));
-        Network network = new Network();
-        int numRouters = Integer.parseInt(scanner.nextLine());
+    private Network network;
+    private PriorityQueue<TopologicalEvent> events;
 
-        for (int i = 1; i <= numRouters; i++) {
-            network.addRouter(new Router(i));
-        }
-        String[] data;
-        while (scanner.hasNextLine()) {
-            data = scanner.nextLine().split(" ");
-            Router routerSource = new Router(Integer.parseInt(data[0]));
-            Router routerDest = new Router(Integer.parseInt(data[1]));
-            int cost = Integer.parseInt(data[2]);
-            network.addLink(routerSource, routerDest);
-            network.setLinkWeight(routerSource, routerDest, cost);
-        }
-
-        return network;
+    private VectorRoutingSim(Network network, PriorityQueue<TopologicalEvent> events) {
+        this.network = network;
+        this.events = events;
     }
 
-    private PriorityQueue<TopologicalEvent> readTopologicalEvents(String fileName) throws Exception {
-        PriorityQueue<TopologicalEvent> pq = new PriorityQueue<>();
+    private void regularRouting(int flag) {
 
-        Scanner scanner = new Scanner(new File(fileName));
-        String[] data;
-        while (scanner.hasNextLine()) {
-            data = scanner.nextLine().split(" ");
-            int roundNum = Integer.parseInt(data[0]);
-            int routerSource = Integer.parseInt(data[1]);
-            int routerDest = Integer.parseInt(data[2]);
-            int cost = Integer.parseInt(data[3]);
-            pq.add(new TopologicalEvent(roundNum, routerSource, routerDest, cost));
-        }
-
-        return pq;
     }
+
+    private void splitHorizon(int flag) {
+
+    }
+
+    private void poisonReverse(int flag) {
+
+    }
+
+
 
     public static void main(String[] args) {
 
         if (args.length != 3) {
             System.out.println("Incorrect arguments. Please try again!");
         }
-        
+
         String[] routingAlgorithms = new String[]{"Regular", "Split Horizon", "Poison Reverse"};
 
-        VectorRoutingSim simulator = new VectorRoutingSim();
-
+        FileParser fileParser = new FileParser();
         String networkFile = args[0].trim();
         String eventFile = args[1].trim();
         int flag = Integer.parseInt(args[2].trim());
@@ -65,13 +48,13 @@ public class VectorRoutingSim {
         Network originalNetwork = new Network();
 
         try {
-            originalNetwork = simulator.readNetwork(networkFile);
+            originalNetwork = fileParser.readNetwork(networkFile);
         } catch (Exception e) {
             System.out.println("Error creating network. Please check the file.");
         }
 
         try {
-            originalEvents = simulator.readTopologicalEvents(eventFile);
+            originalEvents = fileParser.readTopologicalEvents(eventFile);
         } catch (Exception e) {
             System.out.println("Error reading topological events. Please check the file.");
             System.exit(1);
@@ -81,6 +64,20 @@ public class VectorRoutingSim {
             System.out.println("ALGORITHM: " + algorithm);
             Network network = new Network(originalNetwork);
             PriorityQueue<TopologicalEvent> events = new PriorityQueue<>(originalEvents);
+            VectorRoutingSim simulator = new VectorRoutingSim(network, events);
+
+            switch (algorithm) {
+                case "Regular":
+                    simulator.regularRouting(flag);
+                    break;
+                case "Split Horizon":
+                    simulator.splitHorizon(flag);
+                    break;
+                case "Poison Reverse":
+                    simulator.poisonReverse(flag);
+                    break;
+            }
+
         }
 
     }
